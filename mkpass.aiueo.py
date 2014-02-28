8adffbe810a7964c19611b6acf64a9ee5bcf6070
syakesaba <syakesaba@gmail.com>
2014-02-28 13:15:06 +0900

    日本語は母音と子音の組み合わせ。

---
+++ mkpass.aiueo.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding utf-8
+
+import string
+a2z = string.lowercase
+aiueo = "aiueo"
+print "\n".join([s1+a1+s2+a2+s3+a3 for s1 in a2z\
+                             for a1 in aiueo\
+                             for s2 in a2z\
+                             for a2 in aiueo\
+                             for s3 in a2z\
+                             for a3 in aiueo])
